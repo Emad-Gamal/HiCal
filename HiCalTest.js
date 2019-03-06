ac1edf7a4994dbeecf586567c6829acb85b7cdbb
emad-gamal <emad.gamal33@gmail.com>
2019-03-06 15:05:42 +0100

    .

--- HiCalTest.js
+++ HiCalTest.js
@@ -1,9 +1,9 @@
 //Test Group 1
-var inputTime1 = [{ startTime: 0, endTime: 1 },
+var inputTime1 = [{ startTime: 4, endTime: 8 },
+{ startTime: 0, endTime: 1 },
 { startTime: 3, endTime: 5 },
-{ startTime: 4, endTime: 8 },
-{ startTime: 9, endTime: 10 },
-{ startTime: 10, endTime: 12 }];
+{ startTime: 10, endTime: 12 },
+{ startTime: 9, endTime: 10 }];
 
 var sortExpected1 = [{ startTime: 0, endTime: 1 },
     { startTime: 3, endTime: 5 },
@@ -35,41 +35,49 @@ var assert = require('assert');
 describe('HiCal.js Functions ', function () {
     describe('#sortTimeSlots(timeSlots) test 1', function () {
         it('Testing sort time slots function with data set inputTime1 ', function () {
+            console.log("Ruuning Test Case:\n",inputTime1);
             var returnedSorted = HiCal.sortTimeSlots(inputTime1);
             for(i=0;i<returnedSorted.length;i++){
                 assert.equal(returnedSorted[i].startTime, sortExpected1[i].startTime);
                 assert.equal(returnedSorted[i].endTime, sortExpected1[i].endTime);
             }
+            console.log("Test returnedSorted:\n",returnedSorted);
         });
     });
 
     describe('#sortTimeSlots(timeSlots) test 2', function () {
         it('Testing sort time slots function with data inputTime2', function () {
+            console.log("Ruuning Test Case:\n",inputTime2);
             var returnedSorted = HiCal.sortTimeSlots(inputTime2);
             for(i=0;i<returnedSorted.length;i++){
                 assert.equal(returnedSorted[i].startTime, sortExpected2[i].startTime);
                 assert.equal(returnedSorted[i].endTime, sortExpected2[i].endTime);
             }
+            console.log("Test returnedSorted:\n",returnedSorted);
         });
     });
 
     describe('#mergeRange(timeSlots) test 1', function () {
         it('Testing sort merge range function with data inputTime1', function () {
+            console.log("Ruuning Test Case:\n",inputTime1);
             var returnedMerged = HiCal.mergeRange(inputTime1);
             for(i=0;i<returnedMerged.length;i++){
                 assert.equal(returnedMerged[i].startTime, mergeExpected1[i].startTime);
                 assert.equal(returnedMerged[i].endTime, mergeExpected1[i].endTime);
             }
+            console.log("Test returnedSorted:\n",returnedMerged);
         });
     });
 
     describe('#mergeRange(timeSlots) test 2', function () {
         it('Testing sort merge range function with data inputTime2', function () {
+            console.log("Ruuning Test Case:\n",inputTime1);
             var returnedMerged = HiCal.mergeRange(inputTime2);
             for(i=0;i<returnedMerged.length;i++){
                 assert.equal(returnedMerged[i].startTime, mergeExpected2[i].startTime);
                 assert.equal(returnedMerged[i].endTime, mergeExpected2[i].endTime);
             }
+            console.log("Test returnedSorted:\n",returnedMerged);
         });
     });
 });
